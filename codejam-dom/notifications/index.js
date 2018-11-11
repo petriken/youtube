8e64bcf6bcc8cecd90146e10e60dc1044457f909
petriken <petriken82@gmail.com>
2018-11-11 20:45:56 +0300

    feat: add new file(js, css)

---
+++ codejam-dom/notifications/index.js
@@ -0,0 +1,34 @@
+// // let mailNotification = new Notification("main notification", {
+// //     tag : "hi",
+// //     body : "Все пропало, Кац предлагает сдаться",
+// //     icon : "http://habrastorage.org/storage2/cf9/50b/e87/cf950be87f8c34e63c07217a009f1d17.jpg"
+// // });
+
+// function func(a) {
+//     let b = eval(a);
+//     return b;
+//   }
+  
+// let res = func("(3+2)+(5+5)*2");
+
+// console.log(res);
+
+// function func(phrase, who) {
+//     alert( phrase + ', ' + who );
+//   }
+  
+//   setTimeout(func, 5000, "Привет", "Вася"); 
+
+//   setTimeout(function() {document.body.children[0].classList.add("notifications")}, 5000);
+setTimeout(function() {document.body.children[0].style.visibility = "visible"}, 5000);
+// style.visibility = "hidden"
+
+//   classlist.toggle включить/выключить класс
+//    elem.appendChiled - добавить node
+// document.createElement('tagname');
+// Creates a new element with the given tag name.
+
+// document.createTextNode('text');
+// creates a new Text node.
+
+// first-notif
